5061ebd4b626358c22f931053a67173afa286fd8
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-07-24 10:35:33 -0300

    add network-test

--- src/functions/database/index.ts
+++ src/functions/database/index.ts
@@ -1,45 +1,11 @@
-import * as Umzug from 'umzug'
-import { makeFromLogger } from '../../lib/logger'
-import { getDatabase } from '../../database'
-
-const makeLogger = makeFromLogger('database/index')
-
-const getMigrationsPath = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return './dist/migrations'
-  }
-
-  return './build/database/migrations'
-}
+import * as Promise from 'bluebird'
+import * as cep from 'cep-promise'
 
 export const migrate = (event, context, callback) => {
-  const logger = makeLogger({ operation: 'migrate' })
-
-  logger.info({ status: 'started' })
-
-  getDatabase().then((database) => {
-    const umzug = new Umzug({
-      storage: 'sequelize',
-      storageOptions: {
-        sequelize: database
-      },
-      migrations: {
-        params: [
-          database.getQueryInterface(),
-          database.constructor
-        ],
-        path: getMigrationsPath(),
-        pattern: /\.js$/
-      }
-    })
-
-    umzug.up()
-      .tap(() => logger.info({ status: 'succeeded' }))
-      .then(() => callback(null))
-      .catch(err => callback(err))
-      .catch((err) => {
-        logger.error({ status: 'failed', metadata: { err } })
-        callback(err)
-      })
-  })
+  return Promise.resolve('01329010')
+    .tap(() => console.log('STARTED'))
+    .then(cep)
+    .tap(console.log)
+    .catch(console.log)
+    .finally(() => console.log('ENDED'))
 }
